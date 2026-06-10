25c5059b1910c985311332c18e759b8d0e62f975
Kyoshi1777 <149602639+Kyoshi1777@users.noreply.github.com>
2026-06-10 14:02:27 -0400

    too high rps

--- src/main/java/frc/robot/subsystems/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/ShooterConstants.java
@@ -4,5 +4,5 @@ public abstract class ShooterConstants {
     public static final int MOTOR_ID = 0;
 
     public static final double SPIN_VOLTAGE = 3;
-    public static final double SPIN_VELOCITY_RPS = 50;
+    public static final double SPIN_VELOCITY_RPS = 5;
 }
