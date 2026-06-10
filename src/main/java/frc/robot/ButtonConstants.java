a368678e53cfa020ad3a4d5a1c1e3513d30254d2
Kyoshi1777 <149602639+Kyoshi1777@users.noreply.github.com>
2026-06-10 13:34:08 -0400

    button constants

--- src/main/java/frc/robot/ButtonConstants.java
+++ src/main/java/frc/robot/ButtonConstants.java
@@ -1,7 +1,11 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class ButtonConstants {
-    public static final JoystickButton a = null;
+    public static final Joystick controller = new Joystick(0);
+    
+    public static final JoystickButton SPIN_VOLTAGE_BUTTON = new JoystickButton(controller, 1);
+    public static final JoystickButton SPIN_VELOCITY_BUTTON = new JoystickButton(controller, 2);
 }
